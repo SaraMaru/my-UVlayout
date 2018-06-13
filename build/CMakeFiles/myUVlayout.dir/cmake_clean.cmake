97c9259a4568df653676985ff50742fd4d337fba
SaraMaru <maijmwq@126.com>
2018-06-13 22:28:29 +0800

    1

---
+++ build/CMakeFiles/myUVlayout.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myUVlayout.dir/viewer.cpp.o"
+  "myUVlayout.pdb"
+  "myUVlayout"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myUVlayout.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
